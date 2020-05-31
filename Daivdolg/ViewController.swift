9052ecbe14b67ea8e0d2387d771dcb8c8e9d113d
Dmitry Bakulin <d.s.bakulin@gmail.com>
2020-05-31 21:37:44 +0300

    Initial Commit

---
+++ Daivdolg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Daivdolg
+//
+//  Created by Dmitry Bakulin on 31/05/2020.
+//  Copyright © 2020 Dmitry Bakulin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
