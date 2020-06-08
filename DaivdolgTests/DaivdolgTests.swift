3591eef68d27a2f872461acd64cd52b3727292bf
4everlad <d.s.bakulin@gmail.com>
2020-06-08 13:06:26 +0300

    add UserDataStorage test

--- DaivdolgTests/DaivdolgTests.swift
+++ DaivdolgTests/DaivdolgTests.swift
@@ -7,27 +7,25 @@
 //
 
 import XCTest
+@testable import Daivdolg
 
 class DaivdolgTests: XCTestCase {
+  
+   var sut: UserDataStorage!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+         sut = UserDataStorage()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+  
+  func testIsAuthenticationRequiredShouldBeTrue() throws {
+    sut.isAuthenticationRequired = true
+    let value = sut.isAuthenticationRequired
+    
+    XCTAssert(value == true, "isAuthenticationRequired should be true")
+  }
 
 }
